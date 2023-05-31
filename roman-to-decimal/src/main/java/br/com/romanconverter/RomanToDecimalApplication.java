317621713a4215c51270bcdc8a75cf741e9daac5
ElielRegiani <elielregiani@gmail.com>
2023-05-30 22:42:09 -0300

    Initializing Spring Project

---
+++ roman-to-decimal/src/main/java/br/com/romanconverter/RomanToDecimalApplication.java
@@ -0,0 +1,13 @@
+package br.com.romanconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanToDecimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomanToDecimalApplication.class, args);
+	}
+
+}
---
+++ roman-to-decimal/src/test/java/br/com/romanconverter/RomanToDecimalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.romanconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomanToDecimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
