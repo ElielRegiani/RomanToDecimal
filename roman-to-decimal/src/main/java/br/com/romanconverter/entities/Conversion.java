e5ffaaa63492152e9f4f3002ef5c04c216d70dc2
ElielRegiani <elielregiani@gmail.com>
2023-06-01 20:48:09 -0300

    Creating Models

---
+++ roman-to-decimal/src/main/java/br/com/romanconverter/entities/Conversion.java
@@ -0,0 +1,51 @@
+package br.com.romanconverter.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CONVERSION")
+public class Conversion {
+
+    private Long id;
+    private String romanNumber;
+    private String decimalNumber;
+
+    public Conversion(Long id, String romanNumber, String decimalNumber) {
+        this.id = id;
+        this.romanNumber = romanNumber;
+        this.decimalNumber = decimalNumber;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "ROMAN_NUMBER")
+    public String getRomanNumber() {
+        return romanNumber;
+    }
+
+    public void setRomanNumber(String romanNumber) {
+        this.romanNumber = romanNumber;
+    }
+
+    @Column(name = "DECIMAL_NUMBER")
+    public String getDecimalNumber() {
+        return this.decimalNumber;
+    }
+
+    public void setDecimalNumber(String decimalNumber) {
+        this.decimalNumber = decimalNumber;
+    }
+}
---
+++ roman-to-decimal/src/main/java/br/com/romanconverter/entities/ConversionHistory.java
@@ -0,0 +1,50 @@
+package br.com.romanconverter.entities;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CONVERSION_HISTORY")
+public class ConversionHistory {
+
+    private Long id;
+    private Date date;
+    private List<Conversion> conversions;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "DATE")
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    public List<Conversion> getConversions() {
+        return conversions;
+    }
+
+    public void setConversions(List<Conversion> conversions) {
+        this.conversions = conversions;
+    }
+}
